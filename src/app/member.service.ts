fc730b70d1661528f5e9661c62a1cc115ee14136
sinceseptember4 <sinceseptember4@gmail.com>
2022-02-07 18:13:22 +0900

    end of search

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -64,6 +64,17 @@ export class MemberService {
       );
   }
 
+  searchMembers(term: string): Observable<Member[]> {
+    if(!term.trim()) {
+      return of([]);
+    }
+    return this.http.get<Member[]>(`${term}にマッチする社員データが見つかりました。`)
+      .pipe(
+        tap(_ => this.log(`${term}にマッチするデータが見つかりました。`)),
+        catchError(this.handleError<Member[]>('searchMember',[]))
+      );
+  }
+
   private log(message: string) {
     this.messageService.add(`MemberService: ${message}`);
   }
